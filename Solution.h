73258ffa17020fe4d548cbb7ab5a5aa3919b953f
YaroslavPetrushko <yaroslavpetrushko.off@gmail.com>
2024-11-09 00:42:29 +0200

    Fixed "extend". Check Solution. Need to debug and optimization

--- Solution.h
+++ Solution.h
@@ -13,5 +13,7 @@ vector<vector<int>> floydWarshalls(vector<vector<pair<int, int>>>& g, int n);
 class Solution {
 public:
 	pair<int, vector<pair<int, int>>>chinesePostmanProblem(vector<vector<int>>& e, int n);
+	pair<int, vector<pair<int, int>>> findShortestPathThroughCommonVertex(int u, int v, const vector<vector<int>>& shortestPath, int n);
+
 };
 #endif // SOLUTION_H
